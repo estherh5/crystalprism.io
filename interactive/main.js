d0ef95d2604cbc721bfe01b4650b70740827807f
Esther Hoffman <ehoffman@epic.com>
2017-01-22 10:35:45 -0600

    Add heart rain, remove heart creation upon button click

--- interactive/main.js
+++ interactive/main.js
@@ -1,6 +1,5 @@
-document.getElementById('button').onmousedown = createHeart;
-document.getElementById('canvas').onclick = createBlast;
 var canvas = document.getElementById('canvas');
+document.getElementById('canvas').onclick = createBlast;
 function canvasSize() {
   window.addEventListener('resize', resize, false);
   function resize() {
@@ -9,6 +8,34 @@ function canvasSize() {
   }
   resize();
 }
+function createHeart() {
+  var heart = document.getElementById('heart').cloneNode(true);
+  heart.setAttribute('class', 'heart');
+  heart.setAttribute('height', '70px');
+  heart.setAttribute('width', '70px');
+  heart.setAttribute('x', '10px');
+  heart.setAttribute('y', '10px');
+  heart.setAttribute('fill', document.getElementById('text').value);
+  canvas.appendChild(heart);
+  heart.onclick = function () {
+    heart.remove();
+  };
+  return heart;
+}
+function createRain() {
+  for(var i = 0; i < 10; i++){
+    var heart = createHeart();
+    heart.setAttribute('x', i*200 + 'px');
+  };
+}
+setInterval(createRain, 1500);
+setInterval(function () {
+  var hearts = document.getElementsByClassName('heart');
+  for(var i = 0; i < hearts.length; i++){
+    var inty = parseFloat(hearts[i].getAttribute('y'));
+    hearts[i].setAttribute('y', inty + 1 + 'px');
+  }
+}, 10);
 function createBlast(e) {
   var blast = document.getElementById('blast').cloneNode(true);
   var x = (e.x - e.target.getBoundingClientRect().left - 32) + 'px';
@@ -18,34 +45,8 @@ function createBlast(e) {
   blast.setAttribute('x', x);
   blast.setAttribute('y', y);
   blast.setAttribute('fill', document.getElementById('text').value);
+  canvas.appendChild(blast);
   setTimeout(function () {
     blast.remove();
   }, 500);
-  canvas.appendChild(blast);
 }
-function createHeart(e) {
-  var heart = document.getElementById('heart').cloneNode(true);
-  var x = e.x - e.target.getBoundingClientRect().left;
-  var y = e.y - e.target.getBoundingClientRect().top;
-  heart.setAttribute('class', 'heart');
-  heart.setAttribute('height', '70px');
-  heart.setAttribute('width', '70px');
-  heart.setAttribute('x', x);
-  heart.setAttribute('y', y);
-  heart.setAttribute('fill', document.getElementById('text').value);
-  heart.onclick = function () {
-    heart.remove();
-  }
-  canvas.appendChild(heart);
-}
-setInterval(function () {
-  var hearts = document.getElementsByClassName('heart');
-  for(var i = 0; i < hearts.length; i++){
-    var randomxIncrement = Math.random() * 100;
-    var randomyIncrement = Math.random() * 100;
-    var intx = parseInt(hearts[i].getAttribute('x'));
-    var inty = parseInt(hearts[i].getAttribute('y'));
-    hearts[i].setAttribute('x', intx + randomxIncrement + 'px');
-    hearts[i].setAttribute('y', inty + randomyIncrement + 'px');
-  }
-}, 2000);

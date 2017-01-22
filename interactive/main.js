a608803000c46c7336a129fca85efe8458329b50
estherh5 <hoffman.esther@gmail.com>
2017-01-21 21:51:30 -0600

    Update increment variable name, add blast on click and disappear after 1000 ms, add heart remove on click

--- interactive/main.js
+++ interactive/main.js
@@ -1,5 +1,5 @@
-document.getElementById('button').onmousedown = createCircle;
-document.getElementById('canvas').onclick = createHeart;
+document.getElementById('button').onmousedown = createHeart;
+document.getElementById('canvas').onclick = createBlast;
 var canvas = document.getElementById('canvas');
 function canvasSize() {
   window.addEventListener('resize', resize, false);
@@ -9,48 +9,43 @@ function canvasSize() {
   }
   resize();
 }
+function createBlast(e) {
+  var blast = document.getElementById('blast').cloneNode(true);
+  var x = (e.x - e.target.getBoundingClientRect().left - 32) + 'px';
+  var y = (e.y - e.target.getBoundingClientRect().top - 25) + 'px';
+  blast.setAttribute('height', '70px');
+  blast.setAttribute('width', '70px');
+  blast.setAttribute('x', x);
+  blast.setAttribute('y', y);
+  blast.setAttribute('fill', document.getElementById('text').value);
+  setTimeout(function () {
+    blast.remove();
+  }, 500);
+  canvas.appendChild(blast);
+}
 function createHeart(e) {
   var heart = document.getElementById('heart').cloneNode(true);
   var x = e.x - e.target.getBoundingClientRect().left;
   var y = e.y - e.target.getBoundingClientRect().top;
   heart.setAttribute('class', 'heart');
-  heart.setAttribute('height', '32px');
-  heart.setAttribute('width', '32px');
+  heart.setAttribute('height', '70px');
+  heart.setAttribute('width', '70px');
   heart.setAttribute('x', x);
   heart.setAttribute('y', y);
   heart.setAttribute('fill', document.getElementById('text').value);
+  heart.onclick = function () {
+    heart.remove();
+  }
   canvas.appendChild(heart);
 }
-function createCircle() {
-  var shape = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  var randomWidth = Math.random() * 1000;
-  var randomHeight = Math.random() * 1000;
-  shape.setAttribute('class', 'circle');
-  shape.setAttribute('cx', randomWidth + 'px');
-  shape.setAttribute('cy', randomHeight + 'px');
-  shape.setAttribute('r', '10px');
-  shape.setAttribute('fill', document.getElementById('text').value);
-  canvas.appendChild(shape);
-}
-setInterval(function () {
-  var circles = document.getElementsByClassName('circle');
-  for(var i = 0; i < circles.length; i++){
-    var randomIncrementOne = Math.random() * 100;
-    var randomIncrementTwo = Math.random() * 100;
-    var intcx = parseInt(circles[i].getAttribute('cx'));
-    var intcy = parseInt(circles[i].getAttribute('cy'));
-    circles[i].setAttribute('cx', intcx + randomIncrementOne + 'px');
-    circles[i].setAttribute('cy', intcy + randomIncrementTwo + 'px');
-  }
-}, 75);
 setInterval(function () {
   var hearts = document.getElementsByClassName('heart');
   for(var i = 0; i < hearts.length; i++){
-    var randomIncrementOne = Math.random() * 100;
-    var randomIncrementTwo = Math.random() * 100;
+    var randomxIncrement = Math.random() * 100;
+    var randomyIncrement = Math.random() * 100;
     var intx = parseInt(hearts[i].getAttribute('x'));
     var inty = parseInt(hearts[i].getAttribute('y'));
-    hearts[i].setAttribute('x', intx + randomIncrementOne + 'px');
-    hearts[i].setAttribute('y', inty + randomIncrementTwo + 'px');
+    hearts[i].setAttribute('x', intx + randomxIncrement + 'px');
+    hearts[i].setAttribute('y', inty + randomyIncrement + 'px');
   }
-}, 75);
+}, 2000);

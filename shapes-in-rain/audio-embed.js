200fbb79a749620bc6fe182033b2475325419d26
estherh5 <hoffman.esther@gmail.com>
2017-07-21 15:52:13 -0500

    Replace hidden YouTube video with audio only
    - Add new audio toggle button
    - Add new script for audio embed, modified from www.labnol.org
    - Add loading of YouTube iFrame API script

---
+++ shapes-in-rain/audio-embed.js
@@ -0,0 +1,44 @@
+/*
+ YouTube Audio Embed
+ --------------------
+
+ Original Author: Amit Agarwal
+ Web: http://www.labnol.org/?p=26740
+*/
+
+function onYouTubeIframeAPIReady() {
+  var audioDiv = document.getElementById('audio-div');
+  var audioIcon = document.createElement('img');
+  audioIcon.setAttribute('id', 'audio-icon');
+  audioIcon.style.cssText = 'cursor: pointer;';
+  audioDiv.appendChild(audioIcon);
+  var audioPlayer = document.createElement('div');
+  audioPlayer.setAttribute('id', 'audio-player');
+  audioDiv.appendChild(audioPlayer);
+  function audioFunction(audioDiv) {
+    var audioPlayer = audioDiv ? 'on-black.png' : 'off-black.png';
+    audioIcon.setAttribute('src', '../icons/' + audioPlayer);
+  }
+  audioDiv.onclick = function() {
+    audioiFrame.getPlayerState() === YT.PlayerState.PLAYING || audioiFrame.getPlayerState() === YT.PlayerState.BUFFERING ? (audioiFrame.pauseVideo(), audioFunction(!1)) : (audioiFrame.playVideo(), audioFunction(!0));
+  }
+  var audioiFrame = new YT.Player('audio-player', {
+    height: '0',
+    width: '0',
+    videoId: audioDiv.dataset.video,
+    playerVars: {
+      autoplay: audioDiv.dataset.autoplay,
+      loop: audioDiv.dataset.loop,
+      start: 1
+    },
+    events: {
+      onReady: function(audioDiv) {
+        audioiFrame.setPlaybackQuality('small'),
+        audioFunction(audioiFrame.getPlayerState() !== YT.PlayerState.CUED)
+      },
+      onStateChange: function(audioDiv) {
+        audioDiv.data === YT.PlayerState.ENDED && audioFunction(!1)
+      }
+    }
+  })
+}

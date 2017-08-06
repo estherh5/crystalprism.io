89e6958deca1e445f63fedf472f6584e01f49960
estherh5 <hoffman.esther@gmail.com>
2017-08-06 11:08:12 -0500

    Add Go Back and New Post options on entry submit
    - Add Clear button to start entry over
    - Add functions to display new options and hide content on submit

--- thought-book/main.js
+++ thought-book/main.js
@@ -3,7 +3,13 @@ var entryName = document.getElementById('entry-name');
 var entry = document.getElementById('entry');
 var toolbarButtons = document.getElementById('format-tools').getElementsByTagName('button');
 var colorPicker = document.getElementById('color-picker');
+var formatTools = document.getElementById('format-tools');
+var clear = document.getElementById('clear');
 var submit = document.getElementById('submit');
+var journal = document.getElementById('journal');
+var clearSubmit = document.getElementById('clear-submit');
+var goBack = document.getElementById('go-back');
+var newPost = document.getElementById('new-post');
 
 // Define events
 setInterval(saveData, 1000);
@@ -22,7 +28,10 @@ for (var i = 0; i < toolbarButtons.length; i++) {
 
 colorPicker.oninput = executeCommand;
 window.onclick = enterTitle;
+clear.onclick = clearEntry;
 submit.onclick = postEntry;
+goBack.onclick = seeOld;
+newPost.onclick = startNew;
 
 // Define functions
 function saveData() {
@@ -55,6 +64,11 @@ function enterTitle(e) {
   }
 }
 
+function clearEntry() {
+  entryName.value = '[title]';
+  entry.innerHTML = '';
+}
+
 function postEntry() {
   var now = new Date();
   if (window.location.hostname == 'crystalprism.io') {
@@ -80,7 +94,55 @@ function postEntry() {
       method: 'POST',
       body: data,
     })
-    entryName.value = '[title]';
-    entry.innerHTML = '';
+    journal.classList.add('entry-done-journal');
+    formatTools.classList.add('entry-done-content');
+    entry.classList.add('entry-done-content');
+    clearSubmit.classList.add('entry-done-content');
+    goBack.classList.add('entry-done-buttons');
+    newPost.classList.add('entry-done-buttons');
+    setTimeout (function() {
+      journal.style.justifyContent = 'center';
+      formatTools.style.display = 'none';
+      entry.style.display = 'none';
+      clearSubmit.style.display = 'none';
+      goBack.style.display = 'initial';
+      newPost.style.display = 'initial';
+    }, 200);
   }
+}
+
+function seeOld() {
+  journal.classList.remove('entry-done-journal');
+  setTimeout (function() {
+    journal.style.justifyContent = 'flex-start';
+    formatTools.style.display = 'flex';
+    entry.style.display = 'block';
+    clearSubmit.style.display = 'flex';
+    goBack.style.display = 'none';
+    newPost.style.display = 'none';
+    formatTools.classList.remove('entry-done-content');
+    entry.classList.remove('entry-done-content');
+    clearSubmit.classList.remove('entry-done-content');
+    goBack.classList.remove('entry-done-buttons');
+    newPost.classList.remove('entry-done-buttons');
+  }, 200);
+}
+
+function startNew() {
+  journal.classList.remove('entry-done-journal');
+  entryName.value = '[title]';
+  entry.innerHTML = '';
+  setTimeout (function() {
+    journal.style.justifyContent = 'flex-start';
+    formatTools.style.display = 'flex';
+    entry.style.display = 'block';
+    clearSubmit.style.display = 'flex';
+    goBack.style.display = 'none';
+    newPost.style.display = 'none';
+    formatTools.classList.remove('entry-done-content');
+    entry.classList.remove('entry-done-content');
+    clearSubmit.classList.remove('entry-done-content');
+    goBack.classList.remove('entry-done-buttons');
+    newPost.classList.remove('entry-done-buttons');
+  }, 200);
 }

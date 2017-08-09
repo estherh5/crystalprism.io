47f40f02b0c4798c5241787d32f15fbd6edde832
estherh5 <hoffman.esther@gmail.com>
2017-08-08 21:28:41 -0400

    Update quotation syntax for consistency

--- timespace/main.js
+++ timespace/main.js
@@ -18,22 +18,22 @@ function everySecond() {
   var digitThree = time.slice(-3,-2);
   var digitFour = time.slice(-4,-3);
   var digitFive = time.slice(-5,-4);
-  var hours = ("0" + now.getHours()).slice(-2);
-  var minutes = ("0" + now.getMinutes()).slice(-2);
-  var seconds = ("0" + now.getSeconds()).slice(-2);
-  timeDisplay.innerHTML = hours + ":" + minutes + ":" + seconds;
-  circleOne.style.bottom = digitTwo + digitFour + '.' + digitFour + "%";
-  circleOne.style.right = digitFour + digitTwo + '.' + digitOne + "%";
-  circleOne.style.background = "#" + circleOneColor;
-  circleTwo.style.top = digitThree + digitFour + '.' + digitTwo + "%";
-  circleTwo.style.right = digitFour + digitFive + '.' + digitTwo + "%";
-  circleTwo.style.background = "#" + circleTwoColor;
-  circleThree.style.bottom = digitFive + digitFour + '.' + digitThree + "%";
-  circleThree.style.left = digitFour + digitTwo + '.' + digitFive + "%";
-  circleThree.style.background = "#" + circleThreeColor;
-  circleFour.style.top = digitFour + digitOne + '.' + digitThree + "%";
-  circleFour.style.left = digitOne + digitFour + '.' + digitThree + "%";
-  circleFour.style.background = "#" + circleFourColor;
+  var hours = ('0' + now.getHours()).slice(-2);
+  var minutes = ('0' + now.getMinutes()).slice(-2);
+  var seconds = ('0' + now.getSeconds()).slice(-2);
+  timeDisplay.innerHTML = hours + ':' + minutes + ':' + seconds;
+  circleOne.style.bottom = digitTwo + digitFour + '.' + digitFour + '%';
+  circleOne.style.right = digitFour + digitTwo + '.' + digitOne + '%';
+  circleOne.style.background = '#' + circleOneColor;
+  circleTwo.style.top = digitThree + digitFour + '.' + digitTwo + '%';
+  circleTwo.style.right = digitFour + digitFive + '.' + digitTwo + '%';
+  circleTwo.style.background = '#' + circleTwoColor;
+  circleThree.style.bottom = digitFive + digitFour + '.' + digitThree + '%';
+  circleThree.style.left = digitFour + digitTwo + '.' + digitFive + '%';
+  circleThree.style.background = '#' + circleThreeColor;
+  circleFour.style.top = digitFour + digitOne + '.' + digitThree + '%';
+  circleFour.style.left = digitOne + digitFour + '.' + digitThree + '%';
+  circleFour.style.background = '#' + circleFourColor;
 }
 
 setInterval(everySecond, 1000);

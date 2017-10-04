f5f43d876050b4b36f257facc23a7a0590ae4f9c
estherh5 <hoffman.esther@gmail.com>
2017-10-04 14:52:47 -0400

    Update duplciate username modal to text warning
    - Rename storage items, element IDs, class names, variable names, and function names to be more intuitive
    - Move onload functions to JS instead of HTML for integrity

--- user/create-account/main.js
+++ user/create-account/main.js
@@ -1,10 +1,9 @@
 // Define variables
-var usernameInput = document.getElementById('username');
-var passwordInput = document.getElementById('password');
-var confirmPasswordInput = document.getElementById('confirm-password');
+var okay = document.getElementById('okay');
+var usernameInput = document.getElementById('username-input');
+var passwordInput = document.getElementById('password-input');
+var confirmPasswordInput = document.getElementById('confirm-password-input');
 var submit = document.getElementById('submit');
-var retry = document.getElementById('retry');
-var retryTitle = document.getElementById('retry-title');
 if (window.location.hostname == 'crystalprism.io') {
   var server = 'http://13.58.175.191/api';
 } else {
@@ -12,44 +11,58 @@ if (window.location.hostname == 'crystalprism.io') {
 }
 
 // Define events
+window.addEventListener('load', confirmDeletion, false);
+
+confirmPasswordInput.addEventListener('keyup', function(event) {
+  // Call createAccount function when enter key is pressed
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    createAccount();
+  }
+}, false);
+
 passwordInput.onfocusout = assessPassword;
 confirmPasswordInput.onfocusout = assessPasswordMatch;
 submit.onclick = createAccount;
 
 // Define functions
-$(function () {
+$(function() {
   $('[data-toggle="tooltip"]').tooltip();
 })
 
-function displayDeleted() {
-  if (sessionStorage.getItem('cprequest') == 'delete') {
+function confirmDeletion() {
+  if (sessionStorage.getItem('account-request') == 'delete') {
     $(deleted).modal('show');
-    localStorage.removeItem('cpusername');
-    localStorage.removeItem('cptoken');
-    sessionStorage.removeItem('cprequest');
+    okay.focus();
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('account-request');
   }
 }
 
 function assessUsername() {
   var username = usernameInput.value;
-  if (username.length == 0) {
+  if (!/\S/.test(username)) {
     document.getElementById('user-warning-two').style.display = 'none';
+    document.getElementById('user-warning-three').style.display = 'none';
     document.getElementById('user-warning-one').style.display = 'block';
     return false;
   }
   if (!username.match(/^[a-zA-Z0-9_-]+$/)) {
     document.getElementById('user-warning-one').style.display = 'none';
+    document.getElementById('user-warning-three').style.display = 'none';
     document.getElementById('user-warning-two').style.display = 'block';
     return false;
   }
   document.getElementById('user-warning-one').style.display = 'none';
   document.getElementById('user-warning-two').style.display = 'none';
+  document.getElementById('user-warning-three').style.display = 'none';
   return true;
 }
 
 function assessPassword() {
   var password = passwordInput.value;
-  if (password.length == 0) {
+  if (!/\S/.test(password)) {
     document.getElementById('pass-warning-two').style.display = 'none';
     document.getElementById('pass-warning-one').style.display = 'block';
     return false;
@@ -87,23 +100,27 @@ function createAccount() {
       headers: {'Content-Type': 'application/json'},
       method: 'POST',
       body: data,
-    }).catch(function (error) {
+    }).catch(function(error) {
       window.alert('Your request did not go through. Please try again soon.');
     }).then(function(response) {
-      response.text().then(function (text) {
+      response.text().then(function(text) {
         if (text == 'Username already exists') {
-          $(retry).modal('show');
-          retryTitle.innerHTML = 'Username "' + username + '" already exists';
+          document.getElementById('user-warning-three').style.display = 'block';
         } else if (text == 'Success') {
           return fetch(server + '/login', {
             method: 'GET',
             headers: {'Authorization': 'Basic ' + btoa(username + ':' + password)}
-          }).then(function (response) {
+          }).then(function(response) {
             if (response.status == 200) {
-              response.json().then(function (json_token) {
-                localStorage.setItem('cpusername', username);
-                localStorage.setItem('cptoken', json_token['token']);
-                sessionStorage.setItem('cprequest', 'createaccount');
+              response.json().then(function(json_token) {
+                localStorage.removeItem('username');
+                localStorage.setItem('username', username);
+                localStorage.removeItem('token');
+                localStorage.setItem('token', json_token['token']);
+                sessionStorage.setItem('account-request', 'createaccount');
+                usernameInput.value = '';
+                passwordInput.value = '';
+                confirmPasswordInput.value = '';
                 window.location = '../my-account/index.html';
               })
             }

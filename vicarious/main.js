893cca159eb8a3cbf63c8d23cb6df440e221bd9c
estherh5 <hoffman.esther@gmail.com>
2017-10-04 14:42:25 -0400

    Replace image modal with carousel
    - Move onload functions to JS instead of HTML for integrity
    - Add comments identifying keys pressed to trigger functions
    - Add condition to prevent unnecessary server request for checkAccountStatus function is user does not have a token stored locally
    - Add error modal if Reddit server is down
    - Replace image modal display with carousel display when image is clicked

--- vicarious/main.js
+++ vicarious/main.js
@@ -2,6 +2,7 @@
 var profileLink = document.getElementById('profile-link');
 var accountLink = document.getElementById('account-link');
 var signInLink = document.getElementById('sign-in-link');
+var errorOkayButton = document.getElementById('error-okay');
 var afterValue = '';
 var allImages = [];
 var rightPanel = document.getElementById('right-panel');
@@ -12,8 +13,6 @@ var locationLowercase = '';
 var imageURLs = [];
 var imagesContainer = document.getElementById('images-container');
 var displayedImages = imagesContainer.getElementsByClassName('image');
-var zoomedImage = document.getElementById('zoomed-image');
-var zoomedModal = document.getElementById('zoomed-modal');
 var imageTitleLinks = [];
 var imageTitleTexts = [];
 var imageTitles = imagesContainer.getElementsByClassName('image-title');
@@ -22,7 +21,7 @@ var noResultsModal = document.getElementById('no-results-modal');
 var locationPlaceholders = ['Spain', 'Switzerland', 'India', 'Thailand', 'Italy', 'Canada', 'Norway'];
 var carousel = document.getElementById('carousel');
 var carouselCloseButton = document.getElementById('carousel-close');
-var okayButton = document.getElementById('okay');
+var noResultsOkayButton = document.getElementById('no-results-okay');
 var submitButton = document.getElementById('submit');
 var titleLink = document.getElementById('title-link');
 var noResultsCloseButton = document.getElementById('no-results-modal-close');
@@ -38,14 +37,24 @@ if (window.location.hostname == 'crystalprism.io') {
 }
 
 // Define events
+window.addEventListener('load', checkAccountStatus, false);
+window.addEventListener('load', storeImages, false);
+window.addEventListener('load', setLocationPlaceholder, false);
+
 window.addEventListener('keyup', function(event) {
+  // Click carouselCloseButton when Esc key is pressed
   if (carousel.style.display != 'none' && event.keyCode == 27) {
     event.preventDefault();
     carouselCloseButton.click();
+  // Click noResultsCloseButton when Esc key is pressed
+  } else if (noResultsModal.classList.contains('show') && event.keyCode == 27) {
+    event.preventDefault();
+    noResultsCloseButton.click();
   }
 }, false);
 
 locationInput.addEventListener('keyup', function(event) {
+  // Click submitButton when enter key is pressed
   if (!noResultsModal.classList.contains('show') && event.keyCode == 13) {
     event.preventDefault();
     submitButton.click();
@@ -54,10 +63,14 @@ locationInput.addEventListener('keyup', function(event) {
 
 titleLink.onclick = clearImages;
 submitButton.onclick = clearImages;
-okayButton.onclick = displayRandomImages;
+noResultsOkayButton.onclick = displayRandomImages;
 noResultsCloseButton.onclick = displayRandomImages;
 noResultsModal.onclick = displayRandomImages;
-viewButton.onclick = showCarousel;
+
+viewButton.addEventListener('click', function() {
+  showCarousel(0);
+}, false);
+
 carouselCloseButton.onclick = hideCarousel;
 
 for (var i = 0; i < carouselItems.length; i++) {
@@ -66,39 +79,52 @@ for (var i = 0; i < carouselItems.length; i++) {
 
 // Define functions
 function checkAccountStatus() {
-  return fetch(server + '/user/verify', {
-    headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
-    method: 'GET',
-  }).catch(function(error) {
+  if (localStorage.getItem('token') == null) {
     accountLink.innerHTML = 'Create Account';
     signInLink.innerHTML = 'Sign In';
     signInLink.onclick = function() {
       sessionStorage.setItem('previous-window', '../../vicarious/index.html');
     }
-  }).then(function(response) {
-    if (response.ok) {
-      profileLink.innerHTML = localStorage.getItem('username');
-      profileLink.href = '../user/index.html?username=' + localStorage.getItem('username');
-      accountLink.innerHTML = 'My Account';
-      accountLink.href = '../user/my-account/index.html';
-      signInLink.innerHTML = 'Sign Out';
-      signInLink.onclick = function() {
-        sessionStorage.setItem('account-request', 'logout');
-      }
-    } else {
-      localStorage.removeItem('username');
-      localStorage.removeItem('token');
+  } else {
+    return fetch(server + '/user/verify', {
+      headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
+      method: 'GET',
+    }).catch(function(error) {
       accountLink.innerHTML = 'Create Account';
       signInLink.innerHTML = 'Sign In';
       signInLink.onclick = function() {
         sessionStorage.setItem('previous-window', '../../vicarious/index.html');
       }
-    }
-  })
+    }).then(function(response) {
+      if (response.ok) {
+        profileLink.innerHTML = localStorage.getItem('username');
+        profileLink.href = '../user/index.html?username=' + localStorage.getItem('username');
+        accountLink.innerHTML = 'My Account';
+        accountLink.href = '../user/my-account/index.html';
+        signInLink.innerHTML = 'Sign Out';
+        signInLink.onclick = function() {
+          sessionStorage.setItem('account-request', 'logout');
+        }
+      } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('token');
+        accountLink.innerHTML = 'Create Account';
+        signInLink.innerHTML = 'Sign In';
+        signInLink.onclick = function() {
+          sessionStorage.setItem('previous-window', '../../vicarious/index.html');
+        }
+      }
+    })
+  }
 }
 
 function storeImages() {
-  return fetch('https://www.reddit.com/r/travel.json?limit=100&after=' + afterValue).then(function(response) {
+  return fetch('https://www.reddit.com/r/travel.json?limit=100&after=' + afterValue).catch(function(error) {
+    $(error).modal('show');
+    setTimeout(function() {
+      errorOkayButton.focus();
+    }, 200);
+  }).then(function(response) {
     response.json().then(function(info) {
       afterValue = info['data']['after'];
       for (var i = 0; i < info['data']['children'].length; i++) {
@@ -130,20 +156,19 @@ function displayImages() {
     if (imageURLs.length == 5) {
       rightPanel.classList.remove('cleared');
       loadingImage.classList.remove('loading');
-      locationInput.value = '';
-      setLocationPlaceholder();
       dimmer.style.display = 'none';
+      populateCarousel();
       return;
     }
     if (allImages[i]['title'].toLowerCase().indexOf(locationLowercase) != -1 && imageURLs.includes(allImages[i]['url']) == false) {
       imageURLs.push(allImages[i]['url']);
       var imageNumber = imageURLs.indexOf(allImages[i]['url']);
       displayedImages[imageNumber].src = imageURLs[imageNumber];
+      displayedImages[imageNumber].dataset.number = imageNumber;
       displayedImages[imageNumber].classList.remove('cleared');
-      displayedImages[imageNumber].onclick = function() {
-        zoomedImage.src = this.src;
-        $(zoomedModal).modal('show');
-      }
+      displayedImages[imageNumber].addEventListener('click', function() {
+        showCarousel(this.dataset.number);
+      }, false);
       imageTitleLinks.push('https://reddit.com' + allImages[i]['permalink']);
       imageTitleTexts.push(allImages[i]['title']);
       imageTitles[imageNumber].href = imageTitleLinks[imageNumber];
@@ -155,15 +180,14 @@ function displayImages() {
         noResultsTitle.innerHTML = 'No images found for "' + locationValue + '"';
         $(noResultsModal).modal('show');
         setTimeout(function() {
-          okayButton.focus();
+          noResultsOkayButton.focus();
         }, 200);
       } else if (imageURLs.length != 0) {
         rightPanel.classList.remove('cleared');
         loadingImage.classList.remove('loading');
         dimmer.style.display = 'none';
-        setLocationPlaceholder();
       }
-      locationInput.value = '';
+      populateCarousel();
     }
   }
 }
@@ -186,6 +210,7 @@ function displayRandomImages(e) {
         locationInput.value = '';
         setLocationPlaceholder();
         dimmer.style.display = 'none';
+        populateCarousel();
         return;
       }
       if (!imageURLs.includes(allImages[randomNumber + i]['url'])) {
@@ -223,10 +248,7 @@ function clearImages() {
   }
 }
 
-function showCarousel() {
-  profileLink.style.color = '#ffffff';
-  accountLink.style.color = '#ffffff';
-  signInLink.style.color = '#ffffff';
+function populateCarousel() {
   carouselInner.innerHTML = '';
   for (var i = 0; i < imageURLs.length; i++) {
     if (imageURLs.length < 2) {
@@ -238,9 +260,6 @@ function showCarousel() {
     }
     var carouselItem = document.createElement('div');
     carouselItem.classList.add('carousel-item');
-    if (i == 0) {
-      carouselItem.classList.add('active');
-    }
     var carouselImage = document.createElement('img');
     carouselImage.classList.add('carousel-image');
     carouselImage.classList.add('img-fluid');
@@ -248,8 +267,22 @@ function showCarousel() {
     carouselItem.append(carouselImage);
     carouselInner.append(carouselItem);
   }
-  carousel.style.display = 'initial';
+}
+
+function showCarousel(number) {
+  var carouselItems = document.getElementsByClassName('carousel-item');
+  for (var i = 0; i < carouselItems.length; i++) {
+    carouselItems[i].classList.remove('active');
+  }
+  carouselItems[number].classList.add('active');
+  profileLink.style.color = '#ffffff';
+  accountLink.style.color = '#ffffff';
+  signInLink.style.color = '#ffffff';
+  carousel.style.display = 'flex';
   $('.carousel').carousel('cycle');
+  setTimeout(function() {
+    carouselNext.focus();
+  }, 200);
 }
 
 function hideCarousel(e) {

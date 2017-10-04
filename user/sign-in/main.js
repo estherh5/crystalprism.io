6874b9466acbee1b413ee26f90c46116e5faf473
estherh5 <hoffman.esther@gmail.com>
2017-10-04 14:53:53 -0400

    Update element names and move functions out of HTML
    - Rename storage items, element IDs, class names, variable names, and function names to be more intuitive
    - Move onload functions to JS instead of HTML for integrity

--- user/sign-in/main.js
+++ user/sign-in/main.js
@@ -1,10 +1,9 @@
 // Define variables
-var logout = document.getElementById('logout');
+var logoutOkay = document.getElementById('logout-okay');
+var usernameInput = document.getElementById('username-input');
+var passwordInput = document.getElementById('password-input');
 var submit = document.getElementById('submit');
-var usernameInput = document.getElementById('username');
-var passwordInput = document.getElementById('password');
-var blanks = document.getElementById('blanks');
-var credFail = document.getElementById('cred-fail');
+var incorrectOkay = document.getElementById('incorrect-okay');
 if (window.location.hostname == 'crystalprism.io') {
   var server = 'http://13.58.175.191/api';
 } else {
@@ -12,21 +11,40 @@ if (window.location.hostname == 'crystalprism.io') {
 }
 
 // Define events
-submit.onclick = checkAccount;
+window.addEventListener('load', confirmLogout, false);
+
+usernameInput.addEventListener('keyup', function(event) {
+  // Call requestLogin function when enter key is pressed
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    requestLogin();
+  }
+}, false);
+
+passwordInput.addEventListener('keyup', function(event) {
+  // Call requestLogin function when enter key is pressed
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    requestLogin();
+  }
+}, false);
+
+submit.onclick = requestLogin;
 
 // Define functions
-function displayLogout() {
-  if (sessionStorage.getItem('cprequest') == 'logout') {
+function confirmLogout() {
+  if (sessionStorage.getItem('account-request') == 'logout') {
     $(logout).modal('show');
-    localStorage.removeItem('cpusername');
-    localStorage.removeItem('cptoken');
-    sessionStorage.removeItem('cprequest');
+    logoutOkay.focus();
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('account-request');
   }
 }
 
 function assessUsername() {
   var username = usernameInput.value;
-  if (username.length == 0) {
+  if (!/\S/.test(username)) {
     document.getElementById('user-warning').style.display = 'block';
     return false;
   }
@@ -44,7 +62,7 @@ function assessPassword() {
   return true;
 }
 
-function checkAccount() {
+function requestLogin() {
   var username = usernameInput.value;
   var password = passwordInput.value;
   if (!assessUsername() || !assessPassword()) {
@@ -53,22 +71,25 @@ function checkAccount() {
   return fetch(server + '/login', {
     method: 'GET',
     headers: {'Authorization': 'Basic ' + btoa(username + ':' + password)}
-  }).catch(function (error) {
+  }).catch(function(error) {
     window.alert('Your request did not go through. Please try again soon.');
-  }).then(function (response) {
+  }).then(function(response) {
     if (response.status != 200) {
-      $(credFail).modal('show');
+      $(incorrect).modal('show');
+      incorrectOkay.focus();
       usernameInput.value = '';
       passwordInput.value = '';
       return;
     }
     if (response.status == 200) {
-      response.json().then(function (json_token) {
-        localStorage.setItem('cpusername', username);
-        localStorage.setItem('cptoken', json_token['token']);
-        if (sessionStorage.getItem('cppreviouswindow') != null) {
-          window.location = sessionStorage.getItem('cppreviouswindow');
-          sessionStorage.removeItem('cppreviouswindow');
+      response.json().then(function(json_token) {
+        localStorage.removeItem('username');
+        localStorage.setItem('username', username);
+        localStorage.removeItem('token');
+        localStorage.setItem('token', json_token['token']);
+        if (sessionStorage.getItem('previous-window') != null) {
+          window.location = sessionStorage.getItem('previous-window');
+          sessionStorage.removeItem('previous-window');
         } else {
           window.location = '../my-account/index.html';
         }

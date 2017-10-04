c0f9e481839ea139021355e09c390edae8100aba
estherh5 <hoffman.esther@gmail.com>
2017-10-04 14:45:50 -0400

    Update "name" variable to "player"
    - Move onload functions to JS instead of HTML for integrity
    - Add comments identifying keys pressed to trigger functions
    - Add condition to prevent unnecessary server request for checkAccountStatus function if user does not have a token stored locally

--- rhythm-of-life/main.js
+++ rhythm-of-life/main.js
@@ -13,7 +13,7 @@ var diastolic = 70;
 var direction = '';
 var xDown = null;
 var yDown = null;
-var name = '';
+var player = '';
 var relievers = [Snap('#bicycle'), Snap('#yoga'), Snap('#fruit'), Snap('#pill')];
 var stressors = [Snap('#alcohol'), Snap('#salt'), Snap('#cigarette'), Snap('#stress')];
 var heartbeat = new Howl({
@@ -74,20 +74,12 @@ function gameOver() {
 }
 
 // Define events
-window.mobileStyle = function() {
-  (function(a) {if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) mobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  if (mobile) {
-    stylesheet.href = 'style-mobile.css';
-    startScreenMobile.style.visibility = 'visible';
-    startScreenDesktop.style.visibility = 'hidden';
-  } else {
-    startScreenDesktop.style.visibility = 'visible';
-    startScreenMobile.style.visibility = 'hidden';
-  }
-  return mobile;
-};
+window.addEventListener('load', mobileStyle, false);
+window.addEventListener('load', checkAccountStatus, false);
+window.addEventListener('load', getLeaders, false);
 
 document.body.onkeydown = function(e) {
+  // Resume game when space key is pressed
   if (e.key == ' ') {
     e.preventDefault();
     if (gameOver() || readyToStart()) {
@@ -222,11 +214,21 @@ setInterval(getMinutes, 60000);
 setInterval(getSeconds, 1000);
 
 // Define game functions
+function mobileStyle() {
+  (function(a) {if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) mobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  if (mobile) {
+    stylesheet.href = 'style-mobile.css';
+    startScreenMobile.style.visibility = 'visible';
+    startScreenDesktop.style.visibility = 'hidden';
+  } else {
+    startScreenDesktop.style.visibility = 'visible';
+    startScreenMobile.style.visibility = 'hidden';
+  }
+  return mobile;
+}
+
 function checkAccountStatus() {
-  return fetch(server + '/user/verify', {
-    headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
-    method: 'GET',
-  }).catch(function(error) {
+  if (localStorage.getItem('token') == null) {
     accountLink.innerHTML = 'Create Account';
     signInLink.innerHTML = 'Sign In';
     signInLink.onclick = function() {
@@ -234,21 +236,11 @@ function checkAccountStatus() {
     }
     anonymous.style.display = 'block';
     loggedIn.style.display = 'none';
-  }).then(function(response) {
-    if (response.ok) {
-      profileLink.innerHTML = localStorage.getItem('username');
-      profileLink.href = '../user/index.html?username=' + localStorage.getItem('username');
-      accountLink.innerHTML = 'My Account';
-      accountLink.href = '../user/my-account/index.html';
-      signInLink.innerHTML = 'Sign Out';
-      signInLink.onclick = function() {
-        sessionStorage.setItem('account-request', 'logout');
-      }
-      anonymous.style.display = 'none';
-      loggedIn.style.display = 'block';
-    } else {
-      localStorage.removeItem('username');
-      localStorage.removeItem('token');
+  } else {
+    return fetch(server + '/user/verify', {
+      headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
+      method: 'GET',
+    }).catch(function(error) {
       accountLink.innerHTML = 'Create Account';
       signInLink.innerHTML = 'Sign In';
       signInLink.onclick = function() {
@@ -256,8 +248,31 @@ function checkAccountStatus() {
       }
       anonymous.style.display = 'block';
       loggedIn.style.display = 'none';
-    }
-  })
+    }).then(function(response) {
+      if (response.ok) {
+        profileLink.innerHTML = localStorage.getItem('username');
+        profileLink.href = '../user/index.html?username=' + localStorage.getItem('username');
+        accountLink.innerHTML = 'My Account';
+        accountLink.href = '../user/my-account/index.html';
+        signInLink.innerHTML = 'Sign Out';
+        signInLink.onclick = function() {
+          sessionStorage.setItem('account-request', 'logout');
+        }
+        anonymous.style.display = 'none';
+        loggedIn.style.display = 'block';
+      } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('token');
+        accountLink.innerHTML = 'Create Account';
+        signInLink.innerHTML = 'Sign In';
+        signInLink.onclick = function() {
+          sessionStorage.setItem('previous-window', '../../rhythm-of-life/index.html');
+        }
+        anonymous.style.display = 'block';
+        loggedIn.style.display = 'none';
+      }
+    })
+  }
 }
 
 function getLeaders() {
@@ -272,11 +287,11 @@ function getLeaders() {
         leaders[i].append(tableContainerTwo);
         if (leadersList[i].status == 'user') {
           var userLink = document.createElement('a');
-          userLink.href = '../user/index.html?username=' + leadersList[i].name;
-          userLink.append(document.createTextNode(leadersList[i].name));
+          userLink.href = '../user/index.html?username=' + leadersList[i].player;
+          userLink.append(document.createTextNode(leadersList[i].player));
           tableContainerTwo.append(userLink);
         } else {
-          tableContainerTwo.append(document.createTextNode(leadersList[i].name));
+          tableContainerTwo.append(document.createTextNode(leadersList[i].player));
         }
       }
       for (var i = 0; i < leadersList.length; i++) {
@@ -288,11 +303,11 @@ function getLeaders() {
         leadersMobile[i].append(tableContainerTwo);
         if (leadersList[i].status == 'user') {
           var userLink = document.createElement('a');
-          userLink.href = '../user/index.html?username=' + leadersList[i].name;
-          userLink.append(document.createTextNode(leadersList[i].name));
+          userLink.href = '../user/index.html?username=' + leadersList[i].player;
+          userLink.append(document.createTextNode(leadersList[i].player));
           tableContainerTwo.append(userLink);
         } else {
-          tableContainerTwo.append(document.createTextNode(leadersList[i].name));
+          tableContainerTwo.append(document.createTextNode(leadersList[i].player));
         }
       }
     })
@@ -600,11 +615,11 @@ function getSeconds() {
 
 function updateLeaderboard() {
   if (loggedIn.style.display == 'none') {
-    name = nameInput.value;
+    player = nameInput.value;
     var entry = {
       score: hours * 3600 + minutes * 60 + seconds,
       lifespan: ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2),
-      name: name,
+      player: player,
       status: 'anonymous',
     };
     data = JSON.stringify(entry);

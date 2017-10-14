146c75e56df41e99fc46eea785214f31c7f5ea91
estherh5 <hoffman.esther@gmail.com>
2017-10-14 03:05:20 -0400

    Simplify function and move audio script to root folder
    - Add comments for clarity
    - Move audio-embed.js file to root folder to share with Shapes in Rain
    - Load common.js file to HTML
    - Replace PNG images with SVG images

---
+++ audio-embed.js
@@ -0,0 +1,49 @@
+/*
+ YouTube Audio Embed
+ --------------------
+
+ Original Author: Amit Agarwal
+ Web: http://www.labnol.org/?p=26740
+*/
+
+// Define function to run when YouTube video is ready
+function onYouTubeIframeAPIReady() {
+  // Add audio icon to webpage
+  var audioContainer = document.getElementById('audio-container');
+  var audioIcon = document.createElement('img');
+  audioIcon.id = 'audio-icon';
+  audioContainer.append(audioIcon);
+  // Add audio player to webpage
+  var audioPlayer = document.createElement('div');
+  audioPlayer.id = 'audio-player';
+  audioContainer.append(audioPlayer);
+  // Change icon to on/off view depending on if video is playing
+  function audioFunction(playing) {
+    var image = playing ? 'on.svg' : 'off.svg';
+    audioIcon.src = 'images/' + image;
+  }
+  // Play or pause video when audio control is clicked and change icon accordingly
+  audioContainer.onclick = function() {
+    audioiFrame.getPlayerState() === YT.PlayerState.PLAYING || audioiFrame.getPlayerState() === YT.PlayerState.BUFFERING ? (audioiFrame.pauseVideo(), audioFunction(!1)) : (audioiFrame.playVideo(), audioFunction(!0));
+  }
+  // Set attributes of embedded audio player
+  var audioiFrame = new YT.Player('audio-player', {
+    height: '0',
+    width: '0',
+    videoId: audioContainer.dataset.video,
+    playerVars: {
+      autoplay: audioContainer.dataset.autoplay,
+      loop: audioContainer.dataset.loop,
+      start: 1,
+    },
+    events: {
+      onReady: function() {
+        audioiFrame.setPlaybackQuality('small'),
+        audioFunction(audioiFrame.getPlayerState() !== YT.PlayerState.CUED)
+      },
+      onStateChange: function(audioContainer) {
+        audioContainer.data === YT.PlayerState.ENDED && audioFunction(!1)
+      }
+    }
+  });
+}
--- timespace/main.js
+++ timespace/main.js
@@ -1,90 +1,48 @@
-// Define variables
-var profileLink = document.getElementById('profile-link');
-var accountLink = document.getElementById('account-link');
-var signInLink = document.getElementById('sign-in-link');
-var timeDisplay = document.getElementById('time');
-var circleOne = document.getElementById('circle-one');
-var circleTwo = document.getElementById('circle-two');
-var circleThree = document.getElementById('circle-three');
-var circleFour = document.getElementById('circle-four');
-if (window.location.hostname == 'crystalprism.io') {
-  var server = 'http://13.58.175.191/api';
-} else {
-  var server = 'http://localhost:5000/api';
+// Define load functions
+window.onload = function() {
+  // Create page header (from common.js script)
+  createPageHeader();
+  // Check if user is logged in (from common.js script)
+  checkIfLoggedIn();
+  return;
 }
 
-// Define events
-window.addEventListener('load', checkAccountStatus, false);
 
-setInterval(everySecond, 1000);
+// Display current time, move circles, and change circles' color every second
+setInterval(moveThroughTime, 1000);
 
-// Define functions
-function checkAccountStatus() {
-  if (localStorage.getItem('token') == null) {
-    accountLink.innerHTML = 'Create Account';
-    signInLink.innerHTML = 'Sign In';
-    signInLink.onclick = function() {
-      sessionStorage.setItem('previous-window', '../../timespace/index.html');
-    }
-  } else {
-    return fetch(server + '/user/verify', {
-      headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
-      method: 'GET',
-    }).catch(function(error) {
-      accountLink.innerHTML = 'Create Account';
-      signInLink.innerHTML = 'Sign In';
-      signInLink.onclick = function() {
-        sessionStorage.setItem('previous-window', '../../timespace/index.html');
-      }
-    }).then(function(response) {
-      if (response.ok) {
-        profileLink.innerHTML = localStorage.getItem('username');
-        profileLink.href = '../user/index.html?username=' + localStorage.getItem('username');
-        accountLink.innerHTML = 'My Account';
-        accountLink.href = '../user/my-account/index.html';
-        signInLink.innerHTML = 'Sign Out';
-        signInLink.onclick = function() {
-          sessionStorage.setItem('account-request', 'logout');
-        }
-      } else {
-        localStorage.removeItem('username');
-        localStorage.removeItem('token');
-        accountLink.innerHTML = 'Create Account';
-        signInLink.innerHTML = 'Sign In';
-        signInLink.onclick = function() {
-          sessionStorage.setItem('previous-window', '../../timespace/index.html');
-        }
-      }
-    })
-  }
-}
-
-function everySecond() {
+function moveThroughTime() {
+  // Get current time in milliseconds and convert to string
   var now = new Date();
   var time = now.getTime().toString();
-  var circleOneColor = time.slice(-6);
-  var circleTwoColor = time.slice(-7,-1);
-  var circleThreeColor = time.slice(-8,-2);
-  var circleFourColor = time.slice(-9,-3);
+  // Slice time string to get hours, minutes, seconds
+  var hours = ('0' + now.getHours()).slice(-2);
+  var minutes = ('0' + now.getMinutes()).slice(-2);
+  var seconds = ('0' + now.getSeconds()).slice(-2);
+  var timeDisplay = document.getElementById('time');
+  // Display current time in HH:MM:SS format
+  timeDisplay.innerHTML = hours + ':' + minutes + ':' + seconds;
+  // Set circle coordinates and colors based on time string digits
+  var circleOne = document.getElementById('circle-one');
+  var circleTwo = document.getElementById('circle-two');
+  var circleThree = document.getElementById('circle-three');
+  var circleFour = document.getElementById('circle-four');
   var digitOne = time.slice(-1);
   var digitTwo = time.slice(-2,-1);
   var digitThree = time.slice(-3,-2);
   var digitFour = time.slice(-4,-3);
   var digitFive = time.slice(-5,-4);
-  var hours = ('0' + now.getHours()).slice(-2);
-  var minutes = ('0' + now.getMinutes()).slice(-2);
-  var seconds = ('0' + now.getSeconds()).slice(-2);
-  timeDisplay.innerHTML = hours + ':' + minutes + ':' + seconds;
   circleOne.style.bottom = digitTwo + digitFour + '.' + digitFour + '%';
   circleOne.style.right = digitFour + digitTwo + '.' + digitOne + '%';
-  circleOne.style.backgroundColor = '#' + circleOneColor;
+  circleOne.style.backgroundColor = '#' + time.slice(-6);
   circleTwo.style.top = digitThree + digitFour + '.' + digitTwo + '%';
   circleTwo.style.right = digitFour + digitFive + '.' + digitTwo + '%';
-  circleTwo.style.backgroundColor = '#' + circleTwoColor;
+  circleTwo.style.backgroundColor = '#' + time.slice(-7,-1);
   circleThree.style.bottom = digitFive + digitFour + '.' + digitThree + '%';
   circleThree.style.left = digitFour + digitTwo + '.' + digitFive + '%';
-  circleThree.style.backgroundColor = '#' + circleThreeColor;
+  circleThree.style.backgroundColor = '#' + time.slice(-8,-2);
   circleFour.style.top = digitFour + digitOne + '.' + digitThree + '%';
   circleFour.style.left = digitOne + digitFour + '.' + digitThree + '%';
-  circleFour.style.backgroundColor = '#' + circleFourColor;
+  circleFour.style.backgroundColor = '#' + time.slice(-9,-3);
+  return;
 }

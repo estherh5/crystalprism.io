15ba4edcaec2cf9ca63589d6ac1f5e51905c71fd
estherh5 <hoffman.esther@gmail.com>
2018-04-03 18:34:50 -0400

    Update icons to Font Awesome 5
    Update HTML stylesheet attribute order and comments for consistency

--- user/main.js
+++ user/main.js
@@ -257,7 +257,7 @@ function loadDrawings() {
 
             // Create eye icon to display with drawing views
             var viewsIcon = document.createElement('i');
-            viewsIcon.classList.add('fa');
+            viewsIcon.classList.add('fas');
             viewsIcon.classList.add('fa-eye');
 
             // Create text to display number of views
@@ -347,7 +347,7 @@ function displayDrawingLikes(drawingId, likers) {
 
       if (likers[i]['username'] == localStorage.getItem('username')) {
         var likedHeart = document.createElement('i');
-        likedHeart.classList.add('fa');
+        likedHeart.classList.add('fas');
         likedHeart.classList.add('fa-heart');
         likedHeart.dataset.drawinglike = likers[i]['drawing_like_id'];
         likedHeart.dataset.drawing = drawingId;
@@ -364,8 +364,8 @@ function displayDrawingLikes(drawingId, likers) {
 
   // Otherwise, display an empty heart outline
   var unlikedHeart = document.createElement('i');
-  unlikedHeart.classList.add('fa');
-  unlikedHeart.classList.add('fa-heart-o');
+  unlikedHeart.classList.add('far');
+  unlikedHeart.classList.add('fa-heart');
   likeText.parentNode.insertBefore(unlikedHeart, likeText);
   unlikedHeart.dataset.drawing = drawingId;
 
@@ -425,7 +425,7 @@ function updateLikes() {
 
   /* If heart is already filled in, delete drawing like and decrease drawing
   like count */
-  if (this.classList.contains('fa-heart')) {
+  if (this.classList.contains('fas')) {
 
     return fetch(api + '/canvashare/drawing-like/' + this.dataset
       .drawinglike, {
@@ -446,8 +446,8 @@ function updateLikes() {
           if (response.ok) {
             var likeText = heart.nextSibling;
             var currentLikes = likeText.innerHTML;
-            heart.classList.remove('fa-heart');
-            heart.classList.add('fa-heart-o');
+            heart.classList.remove('fas');
+            heart.classList.add('far');
             delete heart.dataset.drawinglike;
             likeText.innerHTML = parseInt(currentLikes) - 1;
             return;
@@ -484,8 +484,8 @@ function updateLikes() {
           var likeText = heart.nextSibling;
           var currentLikes = likeText.innerHTML;
           likeText.innerHTML = parseInt(currentLikes) + 1;
-          heart.classList.remove('fa-heart-o');
-          heart.classList.add('fa-heart');
+          heart.classList.remove('far');
+          heart.classList.add('fas');
           heart.dataset.drawinglike = drawingLikeId;
         });
         return;
